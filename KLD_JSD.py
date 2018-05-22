c4ce2540542e23077b1b44b4200eca780aef9b67
MachineLP <MachineLP@163.com>
2018-05-22 14:20:56 +0800

    Add files via upload

---
+++ KLD_JSD.py
@@ -0,0 +1,38 @@
+# coding=utf-8
+
+#********Jensen-Shannon Divergence**********
+# Zealseeker | www.zealseeker.com          
+#*******************************************
+#class: JSD
+#method: JSD_core(q,p)
+#input:
+#   q=[q1,q2,q3,...]
+#   p=[p1,p2,p3,...]
+#output:
+#    JS divergence between set p and set q
+#*******************************************
+
+from math import log
+from numpy import array
+
+class JSD:
+    def KLD(self,p,q):
+        if 0 in q :
+            raise ValueError
+        return sum(_p * log(_p/_q) for (_p,_q) in zip(p,q) if _p!=0)
+
+    def JSD_core(self,p,q):
+        M = [0.5*(_p+_q) for _p,_q in zip(p,q)]
+        return 0.5*self.KLD(p,M)+0.5*self.KLD(q,M)
+
+#********Test*******
+p = [2,3,4,1,1]
+q = [3,3,2,1,0]
+
+jsd = JSD()
+print (jsd.JSD_core(p,q))
+print (jsd.JSD_core(q,p))
+
+#--Result--
+#0.566811410952
+#0.566811410952
