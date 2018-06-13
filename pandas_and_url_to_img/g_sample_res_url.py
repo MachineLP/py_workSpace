9a8aea7bc3acdb69dd467e79fef79d2964348dbc
MachineLP <MachineLP@163.com>
2018-06-13 08:29:36 +0800

    Add files via upload

---
+++ pandas_and_url_to_img/g_sample_res_url.py
@@ -0,0 +1,62 @@
+# coding=utf-8
+import pandas as pd  
+import json
+
+dic ={}
+def json_txt(dic_json):
+    if isinstance(dic_json,dict): #判断是否是字典类型isinstance 返回True false     
+        for key in dic_json:
+            if isinstance(dic_json[key],dict):#如果dic_json[key]依旧是字典类型
+                print("****key--：%s value--: %s"%(key,dic_json[key]))
+                json_txt(dic_json[key])
+                dic[key] = dic_json[key]
+            else:
+                print("****key--：%s value--: %s"%(key,dic_json[key]))
+                dic[key] = dic_json[key]
+
+# 以下测试用
+if __name__ == '__main__':
+    File = open("sample_res.csv", "w")
+
+    ############################################ 模版表 ##########################################
+    # 这一部分对应的模版图
+    pd_data_sample = pd.read_csv("模板表-material.csv")  
+    sample_img_id = pd_data_sample['aterial_id']
+    sample_img_url = pd_data_sample['preview 预览图：封面、1张预览、2张预览、3张预览']
+    # sample_img_id = json.loads(sample_img_id.to_json())
+    # sample_img_url = json.loads(sample_img_url.to_json())
+    print (sample_img_id.values)
+    print (sample_img_url.values)
+
+
+    ############################################ 作品表 ##########################################
+    #  这一部分对应的是作品表
+    pd_data_res = pd.read_csv('作品表-work.csv')[:100]
+    # print (pd_data_res.pop('作品内容'))
+    # 转为json的格式
+    res_dic_json = json.loads(pd_data_res['作品内容'].to_json())
+    for res_key in res_dic_json:
+        res_url_id = res_dic_json[res_key]
+        # 作品表的id
+        res_img_id = json.loads(res_url_id)['material_id']
+        if res_img_id == 0:
+            continue
+        # 作品表的URL
+        res_img_url = json.loads(res_url_id)['picture']
+        print (res_img_id)
+    
+        # 用于保存模版的url。
+        # sample_dic_url_list = []
+        # print ( '>>>>', sample_img_url.values[res_img_id-1] )
+        # 减1的目的因为数组从0开始的。
+        sample_dic_jason = json.loads(sample_img_url.values[res_img_id-1])
+        for sample_key in sample_dic_jason:
+            print ('>>>>>>>>>>',sample_dic_jason[sample_key])
+            # sample_dic_url_list.append(sample_dic_jason[sample_key])
+            File.write(str(sample_dic_jason[sample_key]) + ' ')
+        File.write(str(res_img_url) + '\n' )
+    # 生成每行包含多个模版和一个作品
+    File.close()
+
+    
+
---
+++ pandas_and_url_to_img/get_img_from_url.py
@@ -0,0 +1,50 @@
+# coding=utf-8
+import pandas as pd  
+import json
+import os
+from urllib import request
+
+def save_img(img_url,file_name,file_path='img'):
+    #保存图片到磁盘文件夹 file_path中，默认为当前脚本运行目录下的 book\img文件夹
+    try:
+        if not os.path.exists(file_path):
+            print ('文件夹',file_path,'不存在，重新建立')
+            #os.mkdir(file_path)
+            os.makedirs(file_path)
+        #获得图片后缀
+        file_suffix = os.path.splitext(img_url)[1]
+        #拼接图片名（包含路径）
+        # filename = '{}{}{}{}'.format(file_path,os.sep,file_name,file_suffix)
+        filename = '{}{}{}{}'.format(file_path,os.sep,file_name,'.jpg')
+        # 下载图片，并保存到文件夹中
+        request.urlretrieve(img_url,filename=filename)
+    except IOError as e:
+        print ('文件操作失败',e)
+    except Exception as e:
+        print ('错误 ：',e)
+
+# 以下测试用
+if __name__ == '__main__':
+    File = open("sample_res.csv", "r")
+    path_i = 0
+    for line in File:
+        img_url= line.split(' ')
+
+        file_path = 'img/' + str(path_i)
+        # 下面保存的是模版
+        sample_img_url = img_url[:-1]
+        for s_img_url in sample_img_url:
+            res_img_name = s_img_url.split('/')[-1].split('.')[:-1][0]
+            save_img(s_img_url, res_img_name,file_path)
+
+        # 下面保存的是作品图
+        res_img_url = img_url[-1]
+        res_img_name = res_img_url.split('/')[-1].split('.')[:-1][0]
+        save_img(res_img_url, res_img_name,file_path)
+        print (line)
+        path_i = path_i + 1
+    
+    File.close()
+
+    
+
