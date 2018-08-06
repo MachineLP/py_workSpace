35c468868a35556e243c71da773dbcbb81b6f2aa
MachineLP <MachineLP@163.com>
2018-08-06 18:02:48 +0800

    Add files via upload

---
+++ pascal_voc_io.py
@@ -0,0 +1,217 @@
+# -*- coding: utf8 -*-
+import sys
+from xml.etree import ElementTree
+from xml.etree.ElementTree import Element, SubElement
+from lxml import etree
+import codecs
+
+XML_EXT = '.xml'
+ENCODE_METHOD = 'utf-8'
+
+class PascalVocWriter:
+
+    def __init__(self, foldername, filename, imgSize,databaseSrc='Unknown', localImgPath=None):
+        self.foldername = foldername
+        self.filename = filename
+        self.databaseSrc = databaseSrc
+        self.imgSize = imgSize
+        self.boxlist = []
+        self.localImgPath = localImgPath
+        self.verified = False
+
+    def prettify(self, elem):
+        """
+            Return a pretty-printed XML string for the Element.
+        """
+        rough_string = ElementTree.tostring(elem, 'utf8')
+        root = etree.fromstring(rough_string)
+        return etree.tostring(root, pretty_print=True, encoding=ENCODE_METHOD).replace("  ".encode(), "\t".encode())
+        # minidom does not support UTF-8
+        '''reparsed = minidom.parseString(rough_string)
+        return reparsed.toprettyxml(indent="\t", encoding=ENCODE_METHOD)'''
+
+    def genXML(self):
+        """
+            Return XML root
+        """
+        # Check conditions
+        if self.filename is None or \
+                self.foldername is None or \
+                self.imgSize is None:
+            return None
+
+        top = Element('annotation')
+        if self.verified:
+            top.set('verified', 'yes')
+
+        folder = SubElement(top, 'folder')
+        folder.text = self.foldername
+
+        filename = SubElement(top, 'filename')
+        filename.text = self.filename
+
+        if self.localImgPath is not None:
+            localImgPath = SubElement(top, 'path')
+            localImgPath.text = self.localImgPath
+
+        source = SubElement(top, 'source')
+        database = SubElement(source, 'database')
+        database.text = self.databaseSrc
+
+        size_part = SubElement(top, 'size')
+        width = SubElement(size_part, 'width')
+        height = SubElement(size_part, 'height')
+        depth = SubElement(size_part, 'depth')
+        width.text = str(self.imgSize[1])
+        height.text = str(self.imgSize[0])
+        if len(self.imgSize) == 3:
+            depth.text = str(self.imgSize[2])
+        else:
+            depth.text = '1'
+
+        segmented = SubElement(top, 'segmented')
+        segmented.text = '0'
+        return top
+
+    def addBndBox(self, xmin, ymin, xmax, ymax, name, difficult):
+        bndbox = {'xmin': xmin, 'ymin': ymin, 'xmax': xmax, 'ymax': ymax}
+        bndbox['name'] = name
+        bndbox['difficult'] = difficult
+        self.boxlist.append(bndbox)
+
+    def appendObjects(self, top):
+        for each_object in self.boxlist:
+            object_item = SubElement(top, 'object')
+            name = SubElement(object_item, 'name')
+            try:
+                name.text = unicode(each_object['name'])
+            except NameError:
+                # Py3: NameError: name 'unicode' is not defined
+                name.text = each_object['name']
+            pose = SubElement(object_item, 'pose')
+            pose.text = "Unspecified"
+            truncated = SubElement(object_item, 'truncated')
+            if int(each_object['ymax']) == int(self.imgSize[0]) or (int(each_object['ymin'])== 1):
+                truncated.text = "1" # max == height or min
+            elif (int(each_object['xmax'])==int(self.imgSize[1])) or (int(each_object['xmin'])== 1):
+                truncated.text = "1" # max == width or min
+            else:
+                truncated.text = "0"
+            difficult = SubElement(object_item, 'difficult')
+            difficult.text = str( bool(each_object['difficult']) & 1 )
+            bndbox = SubElement(object_item, 'bndbox')
+            xmin = SubElement(bndbox, 'xmin')
+            xmin.text = str(each_object['xmin'])
+            ymin = SubElement(bndbox, 'ymin')
+            ymin.text = str(each_object['ymin'])
+            xmax = SubElement(bndbox, 'xmax')
+            xmax.text = str(each_object['xmax'])
+            ymax = SubElement(bndbox, 'ymax')
+            ymax.text = str(each_object['ymax'])
+
+    def save(self, targetFile=None):
+        root = self.genXML()
+        self.appendObjects(root)
+        out_file = None
+        if targetFile is None:
+            out_file = codecs.open(
+                self.filename + XML_EXT, 'w', encoding=ENCODE_METHOD)
+        else:
+            out_file = codecs.open(targetFile, 'w', encoding=ENCODE_METHOD)
+
+        prettifyResult = self.prettify(root)
+        out_file.write(prettifyResult.decode('utf8'))
+        out_file.close()
+
+
+class PascalVocReader:
+
+    def __init__(self, filepath):
+        # shapes type:
+        # [labbel, [(x1,y1), (x2,y2), (x3,y3), (x4,y4)], color, color, difficult]
+        self.shapes = []
+        self.filepath = filepath
+        self.verified = False
+        try:
+            self.parseXML()
+        except:
+            pass
+
+    def getShapes(self):
+        return self.shapes
+
+    def addShape(self, label, bndbox, difficult):
+        xmin = int(bndbox.find('xmin').text)
+        ymin = int(bndbox.find('ymin').text)
+        xmax = int(bndbox.find('xmax').text)
+        ymax = int(bndbox.find('ymax').text)
+        points = [(xmin, ymin), (xmax, ymin), (xmax, ymax), (xmin, ymax)]
+        self.shapes.append((label, points, None, None, difficult))
+
+    def parseXML(self):
+        assert self.filepath.endswith(XML_EXT), "Unsupport file format"
+        parser = etree.XMLParser(encoding=ENCODE_METHOD)
+        xmltree = ElementTree.parse(self.filepath, parser=parser).getroot()
+        filename = xmltree.find('filename').text
+        try:
+            verified = xmltree.attrib['verified']
+            if verified == 'yes':
+                self.verified = True
+        except KeyError:
+            self.verified = False
+
+        for object_iter in xmltree.findall('object'):
+            bndbox = object_iter.find("bndbox")
+            label = object_iter.find('name').text
+            # Add chris
+            difficult = False
+            if object_iter.find('difficult') is not None:
+                difficult = bool(int(object_iter.find('difficult').text))
+            self.addShape(label, bndbox, difficult)
+        return True
+
+
+f __name__ == '__main__':
+    # 画出矩形框
+    folder_name = 'lp'
+    img_name = '1.png'
+    img = cv2.imread(img_name)
+    h,w,c = img.shape
+    imgSize = [w, h, c]
+    p_voc = PascalVocWriter(foldername=folder_name, filename=img_name, imgSize=imgSize)
+
+    all_data = load_data_from_json('data_01.json').js_load()
+    ## 
+    img_c = parse_img_from_json(all_data)
+    text_c = parse_text_from_json(all_data)
+    mask_c = parse_mask_from_json(all_data)
+
+    for  c in img_c:
+        x1 = int(c[0])
+        y1 = int(c[1])
+        x2 = int(c[2])
+        y2 = int(c[3])
+        cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
+        p_voc.addBndBox(xmin=x1, ymin=y1, xmax=x2, ymax=y2, name='img', difficult=-1)
+    for c in text_c:
+        x1 = int(c[0])
+        y1 = int(c[1])
+        x2 = int(c[2])
+        y2 = int(c[3])
+        cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
+        p_voc.addBndBox(xmin=x1, ymin=y1, xmax=x2, ymax=y2, name='text', difficult=-1)
+    for c in mask_c:
+        x1 = int(c[0])
+        y1 = int(c[1])
+        x2 = int(c[2])
+        y2 = int(c[3])
+        cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
+        p_voc.addBndBox(xmin=x1, ymin=y1, xmax=x2, ymax=y2, name='mask', difficult=-1)
+
+    # show the output image
+    cv2.imwrite("output.jpg", img)
+    cv2.imshow("Output", img)
+    # cv2.waitKey(0)
+    
+    out_xml = img_name.split('.')[-2] + '.xml'
+    p_voc.save(targetFile=out_xml)
